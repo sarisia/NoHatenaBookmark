6c5429df0cd7247e415a79f8cfefce1776d931f9
sarisia <sarisiaice@gmail.com>
2020-11-10 02:35:17 +0900

    init

---
+++ background.js
@@ -0,0 +1,39 @@
+console.log("background loaded")
+
+chrome.webRequest.onBeforeRequest.addListener(
+    (details) => {
+        const target = extractTargetFromHatena(details.url)
+        console.log(target)
+        if (target) {
+            return {
+                "redirectUrl": target
+            }
+        }
+    },
+    {
+        urls: [ "*://b.hatena.ne.jp/entry*" ]
+    },
+    [ "blocking" ]
+)
+
+function extractTargetFromHatena(urlstr) {
+    let target = ""
+    const url = new URL(urlstr)
+    const urlParam = url.searchParams.get("url")
+
+    if (urlParam) {
+        // if url has a query parameter 'url', decode and use them.
+        target = decodeURIComponent(urlParam)
+    } else {
+        let path = url.pathname.replace(/^\/entry\//, "/")
+        let scheme = "http"
+        if (path.startsWith("/s/")) {
+            path = path.replace(/^\/s\//, "/")
+            scheme = "https"
+        }
+        
+        target = `${scheme}://${path}${url.search}`
+    }
+
+    return target
+}
